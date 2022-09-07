3776f685caa8ae4d5946df9d80f93c855548260d
jonesopolis <davidarector@gmail.com>
2022-09-07 19:41:33 -0400

    more pages

--- piper-web/components/layout.js
+++ piper-web/components/layout.js
@@ -16,36 +16,36 @@ function Layout({ children }) {
 
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></Script>
 
-      <div class="container">
-        <footer class="py-3 my-4">
-          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item">
+      <div className="container">
+        <footer className="py-3 my-4">
+          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+            <li className="nav-item">
               <Link href="/">
-                <a class="nav-link px-2 text-muted">Home</a>
+                <a className="nav-link px-2 text-muted">Home</a>
               </Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link href="/about">
-                <a class="nav-link px-2 text-muted">About</a>
+                <a className="nav-link px-2 text-muted">About</a>
               </Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link href="/services">
-                <a class="nav-link px-2 text-muted">Services</a>
+                <a className="nav-link px-2 text-muted">Services</a>
               </Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link href="/sample-sites">
-                <a class="nav-link px-2 text-muted">Sample Sites</a>
+                <a className="nav-link px-2 text-muted">Sample Sites</a>
               </Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link href="/contact">
-                <a class="nav-link px-2 text-muted">Contact</a>
+                <a className="nav-link px-2 text-muted">Contact</a>
               </Link>
             </li>
           </ul>
-          <p class="text-center text-muted">&copy; {new Date().getFullYear()} Piper Mountain Webs</p>
+          <p className="text-center text-muted">&copy; {new Date().getFullYear()} Piper Mountain Webs</p>
         </footer>
       </div>
     </>
--- piper-web/pages/services.js
+++ piper-web/pages/services.js
@@ -1,5 +1,61 @@
+import Link from "next/link";
+
 function Services() {
-  return <h1>SERVICES!!</h1>;
+  function getCards() {
+    var models = [
+      {
+        id: 1,
+        heading: "You manage web content.",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in facilisis libero, eget congue lorem. Nulla non ligula eget erat aliquam lacinia a eget felis. Nulla turpis sapien, ultricies sit amet suscipit quis, auctor id risus. Donec pellentesque tellus metus, in eleifend lacus euismod eget. Vestibulum vehicula ut metus id vestibulum. `,
+        linkText: `This is a link`,
+        linkRoute: "/services",
+      },
+      {
+        id: 2,
+        heading: "One-stop solutions",
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in facilisis libero, eget congue lorem. Nulla non ligula eget erat aliquam lacinia a eget felis. Nulla turpis sapien, ultricies sit amet suscipit quis, auctor id risus. Donec pellentesque tellus metus, in eleifend lacus euismod eget. Vestibulum vehicula ut metus id vestibulum. `,
+        linkText: "This is the link",
+        linkRoute: "/services",
+      },
+      {
+        id: 3,
+        heading: "Services",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in facilisis libero, eget congue lorem. Nulla non ligula eget erat aliquam lacinia a eget felis. Nulla turpis sapien, ultricies sit amet suscipit quis, auctor id risus. Donec pellentesque tellus metus, in eleifend lacus euismod eget. Vestibulum vehicula ut metus id vestibulum.",
+        linkText: "This is the link",
+        linkRoute: "/services",
+      },
+    ];
+
+    return (
+      <div className="row">
+        {models.map((x) => (
+          <div className="col-lg-4 text-center" key={x.id}>
+            <svg className="bd-placeholder-img rounded-circle mb-4" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#777" />
+              <text x="50%" y="50%" fill="#777" dy=".3em">
+                140x140
+              </text>
+            </svg>
+
+            <h2>{x.heading}</h2>
+            <p>{x.description}</p>
+            <p>
+              <Link href={x.linkRoute}>
+                <a className="btn btn-secondary">{x.linkText}&raquo;</a>
+              </Link>
+            </p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="container marketing pt-5">{getCards()}</div>
+    </>
+  );
 }
 
 export default Services;

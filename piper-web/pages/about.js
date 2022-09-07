5213af9ef496169b0e799f918856236c83270b1e
jonesopolis <davidarector@gmail.com>
2022-09-07 19:42:30 -0400

    more pages

--- piper-web/pages/about.js
+++ piper-web/pages/about.js
@@ -5,10 +5,10 @@ function About() {
 
       <div className="container col-5 pb-5">
         <p>Piper Mountain Webs began developing websites for local businesses, community organizations and libraries in 2003.</p>
-        <p>Public libraries were literally part of the family - PiperWebs founder is married to a library director. It didn't take long to realize that small and mid-sized public libraries were in need of an affordable, professional website solution that didn't require in-depth technical skills.</p>
+        <p>Public libraries were literally part of the family - PiperWebs founder is married to a library director. It didn&apos;t take long to realize that small and mid-sized public libraries were in need of an affordable, professional website solution that didn&apos;t require in-depth technical skills.</p>
         <p>To address this need we created a set of professionally-designed website templates and paired them with a custom content management system designed specifically for public libraries.</p>
         <p>Then we added special book displays, event calendar, and optional website pages that are maintained by our staff - like a weekly display of New York Times bestsellers. All in an effort to help small and mid-sized libraries present relevant, current and interesting content to their patrons.</p>
-        <p>Starting with our local library in 2003, we've since grown to serve over 150 libraries in 26 states. We continue to add new ways for this growing community of libraries to share information, best practices, and even website content.</p>
+        <p>Starting with our local library in 2003, we&apos;ve since grown to serve over 150 libraries in 26 states. We continue to add new ways for this growing community of libraries to share information, best practices, and even website content.</p>
       </div>
     </>
   );
